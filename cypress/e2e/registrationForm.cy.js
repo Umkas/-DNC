73bc89852890f111bf77c1058cd628499929263d
Dmytro Yerchak <umkauskas@gmail.com>
2025-02-13 11:31:23 +0200

    test-case 2 - sipmle version w/o POM

---
+++ cypress/e2e/registrationForm.cy.js
@@ -0,0 +1,47 @@
+describe('Форма регистрации', () => {
+    // beforeEach(() => {
+    //   cy.visit('https://www.wikipedia.org/'); // Заменить на актуальный URL
+    // });
+    beforeEach(() => {
+        Cypress.on('uncaught:exception', (err, runnable) => {
+          return false; // Игнорируем ошибки CORS
+        });
+        cy.intercept('GET', '**/adplus.js', { statusCode: 200, body: '' }).as('blockAds');
+        cy.intercept('GET', '**/cdn-ima.33across.com/**', { statusCode: 200, body: '' }).as('blockThirdParty');
+
+        cy.visit('https://demoqa.com/automation-practice-form'); // Заменить на актуальный URL
+      });
+  
+    it('Успешная регистрация и проверка попапа', () => {
+
+      
+      // Заполняем поле Name1
+      cy.get('input[id="firstName"]').type('Petrik');
+      cy.get('input[id="lastName"]').type('Pjatochkin');
+      // Заполняем поле email
+      cy.get('input[id="userEmail"]').type('test@example.com');
+      cy.get('input[id="userNumber"]').type('0123456789');
+            // Выбираем гендер (например, Male)
+      cy.get('label[for="gender-radio-1"]').click();
+  
+      // Нажимаем кнопку Submit
+      cy.get('.btn.btn-primary#submit').click();
+  
+      // Проверяем, что появился попап с заголовком
+      cy.get('.modal-dialog.modal-lg').should('be.visible');
+      cy.get('.modal-title.h4#example-modal-sizes-title-lg').should('have.text', 'Thanks for submitting the form');
+  
+      cy.get('.table.table-dark.table-striped.table-bordered.table-hover').then(($table) => {
+        console.log($table.html()); // Выведет HTML содержимое таблицы для проверки
+      });
+
+      // Проверяем содержимое HTML-таблицы
+      cy.get('.table.table-dark.table-striped.table-bordered.table-hover').within(() => {
+        cy.contains('td', 'Student Name').next().should('have.text', 'Petrik Pjatochkin');
+        cy.contains('td', 'Email').next().should('have.text', 'test@example.com');      
+        cy.contains('td', 'Mobile').next().should('have.text', '0123456789');
+        cy.contains('td', 'Gender').next().should('have.text', 'Male');
+      });
+    });
+  });
+  
